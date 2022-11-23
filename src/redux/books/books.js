d0030c5a3ef0e279cc60f44071aa99ba055db0bf
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-23 11:02:02 -0500

    Write book actions and reducer using ducks pattern

---
+++ src/redux/books/books.js
@@ -0,0 +1,27 @@
+const ADD_BOOK = 'books/ADD_BOOK';
+const REMOVE_BOOK = 'books/REMOVE_BOOK';
+
+const initialState = [];
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.book];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
