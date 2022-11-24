271225c76f193d5322a651871a1fc6e5a1a601fc
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-24 17:50:29 -0500

    Update store

--- src/redux/configure.js
+++ src/redux/configure.js
@@ -1,7 +1,12 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const store = configureStore(combineReducers(booksReducer, categoriesReducer));
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
 
 export default store;
