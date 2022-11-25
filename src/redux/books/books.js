8ecca32da174c063ea33407681c63e6785aae87e
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-25 15:16:49 -0500

    Use thunk to create removeBookAsync

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -17,19 +17,29 @@ export const addBook = (book) => ({
 });
 
 export const addBookAsync = createAsyncThunk(ADD_BOOK, async (book) => {
-  const response= await fetch(`${API_URL}/books/`, {
+  const result = await fetch(`${API_URL}/books/`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(book)
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(book),
   }).then((response) => response.text())
     .then((result) => (result === 'Created' ? book : null));
+  return result;
 });
-
 
 export const removeBook = (id) => ({
   type: REMOVE_BOOK,
   id,
+});
+
+export const removeBookAsync = createAsyncThunk(REMOVE_BOOK, async (id) => {
+  const result = await fetch(`${API_URL}/books/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'content-Type': 'application/json',
+    },
+  }).then((response) => response.text())
+    .then((result) => (result === 'The book was deleted successfully!' ? id : null));
+  return result;
 });
 
 const booksReducer = (state = initialState, action) => {
